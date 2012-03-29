6656688628e490f3bf2fd2039a714ba75b517b43
Bjarne Holen <bjarneholen@gmail.com>
2012-03-29 08:10:06 +0200

    new version for Go v1.0

--- reader.go
+++ reader.go
@@ -5,9 +5,8 @@
 package latinx
 
 import (
-    "io"
-    "os"
-    "bytes"
+	"bytes"
+	"io"
 )
 
 // A LatinReader reads ISO-8859 streams from underlying reader, 
@@ -16,50 +15,50 @@ import (
 // stream has been written to buffer, a Read(p []byte) from *bytes.Buffer
 // is preformed.
 type LatinReader struct {
-    reader    io.Reader
-    converter *Converter
-    buf       *bytes.Buffer
+	reader    io.Reader
+	converter *Converter
+	buf       *bytes.Buffer
 }
 
 // Initialize a new LatinReader using an underlying io.Reader and one of the
 // available charsets (ISO_8895_1, ISO_8895_2..)
 func NewReader(charset int, r io.Reader) io.Reader {
-    conv, ok := converters[charset]
-    if !ok {
-        return nil
-    }
-    b := bytes.NewBuffer(make([]byte, 1024))
-    b.Truncate(0)
-    return &LatinReader{reader: r, converter: conv, buf: b}
+	conv, ok := converters[charset]
+	if !ok {
+		return nil
+	}
+	b := bytes.NewBuffer(make([]byte, 1024))
+	b.Truncate(0)
+	return &LatinReader{reader: r, converter: conv, buf: b}
 }
 
 // Read from underlying io.Reader and decode to UTF-8 and return result.
-func (r *LatinReader) Read(p []byte) (n int, err os.Error) {
+func (r *LatinReader) Read(p []byte) (n int, err error) {
 
-    var p2, utf8bytes []byte
-    var n2 int
-    var e2, e3 os.Error
+	var p2, utf8bytes []byte
+	var n2 int
+	var e2, e3 error
 
-    p2 = make([]byte, len(p))
-    n2, e2 = r.reader.Read(p2)
+	p2 = make([]byte, len(p))
+	n2, e2 = r.reader.Read(p2)
 
-    if e2 == nil || e2 == os.EOF {
+	if e2 == nil || e2 == io.EOF {
 
-        utf8bytes, e3 = r.converter.Decode(p2[:n2])
+		utf8bytes, e3 = r.converter.Decode(p2[:n2])
 
-        if e3 != nil {
-            return 0, e3
-        }
+		if e3 != nil {
+			return 0, e3
+		}
 
-        r.buf.Write(utf8bytes) // n always len(input), error always nil
+		r.buf.Write(utf8bytes) // n always len(input), error always nil
 
-        return r.buf.Read(p)
+		return r.buf.Read(p)
 
-    } else {
-        copy(p, p2)
-        n = n2
-        err = e2
-    }
+	} else {
+		copy(p, p2)
+		n = n2
+		err = e2
+	}
 
-    return
+	return
 }
